a64f6f565f66de31a7f45af329d2d4b7c460b6c7
DRACARYSPC\Priyank <mpriyank2596@gmail.com>
2023-12-30 00:09:45 -0500

    Add employee functionality added

---
+++ FullStack UI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../../../models/employee.model';
+import { EmployeesService } from '../../../services/employees.service';
+
+@Component({
+  selector: 'app-employees-list',
+  templateUrl: './employees-list.component.html',
+  styleUrl: './employees-list.component.css'
+})
+export class EmployeesListComponent implements OnInit {
+
+  employees: Employee[] = []
+
+  constructor(private employeesService: EmployeesService) { }
+
+  ngOnInit(): void {
+    this.employeesService.getAllEmployees()
+      .subscribe({
+        next: (employees) => {
+          this.employees = employees;
+        },
+        error: (response) => {
+          console.log(response);
+        }
+      })
+  }
+
+}
